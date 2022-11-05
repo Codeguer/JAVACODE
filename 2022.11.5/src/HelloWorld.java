c2a269b175e612765ceef203045474c00d57955d
Codeguer <15946817309@163.com>
2022-11-06 00:00:18 +0800

    java Hello World

---
+++ 2022.11.5/src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: z'x
+ * Date: 2022-11-05
+ * Time: 23:59
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
